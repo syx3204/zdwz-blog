dd3372002e40f9808af1120a4d22b073b7aac681
syx3204 <s_y_x@outlook.com>
2025-03-10 21:38:57 +0800

    Delete js directory

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-// 可以添加一些交互效果，比如点击文章标题时的提示
-document.querySelectorAll('.post h3 a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('即将跳转到文章页面');
-        window.location.href = e.target.href;
-    });
-});
